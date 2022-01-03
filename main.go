d701f2f1a41764a92df00290c89870d7eab54212
Burak Tagtekin <burak.tagtekin@icloud.com>
2022-01-03 21:00:27 +0300

    Detailed Callback Function

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func init() {
 }
 
 func main() {
+
 	// Instantiate a GA with a GAConfig
 	var ga, err = eaopt.NewDefaultGAConfig().NewGA()
 	if err != nil {
@@ -23,10 +24,12 @@ func main() {
 
 	// Set the number of generations to run for
 	ga.NGenerations = 10
-
+	ga.ParallelEval = true
 	// Add a custom print function to track progress
 	ga.Callback = func(ga *eaopt.GA) {
-		fmt.Printf("Best fitness at generation %d: %f, %s\n", ga.Generations, ga.HallOfFame[0].Fitness, ga.HallOfFame[0].Genome)
+		hof := ga.HallOfFame[0].Genome
+		fmt.Printf("Best Combination of Flags : %f\n", hof.(scripts.SingleBench).BinVec)
+		fmt.Printf("Best fitness at generation %d: %f\n", ga.Generations, ga.HallOfFame[0].Fitness)
 	}
 
 	// Find the minimum
