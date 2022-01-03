05d824e32fe5e8a9fc7e46c7565b2c0c04ae9fde
Burak Tagtekin <burak.tagtekin@icloud.com>
2022-01-03 20:57:25 +0300

    Existing folder check module added.

--- utils/initializator.go
+++ utils/initializator.go
@@ -2,22 +2,38 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 // TODO : Add UnitTest for this function
 func Initialization(folderName string) {
-	newPath := path.Join(ResultsPath, folderName)
+	newPath := filepath.Join(ResultsPath, folderName)
 	if _, err := os.Stat(newPath); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(newPath, os.ModePerm)
+		err := os.MkdirAll(newPath, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
 		log.Println("File Generation Successful")
 	} else {
-		log.Panicln(" File Exist ! See You ...")
+		fmt.Println("Folder Exist. Would you like to delete and generate again ?")
+		fmt.Println("[1] For Yes, Enter 1")
+		fmt.Println("[2] For No,  Enter 2")
+		var answer string
+		answer = "1"
+		if answer == "1" {
+			os.RemoveAll(newPath)
+			err = os.MkdirAll(newPath, os.ModePerm)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("File Deletion -> Generation Successfull")
+		} else {
+			log.Panic("Quitting. Goodbye ...")
+		}
+
 	}
 
 }
