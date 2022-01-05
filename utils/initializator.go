48644b4dc9950e05f18e064ef029a89124a15297
Burak Tagtekin <burak.tagtekin@icloud.com>
2022-01-05 22:40:27 +0300

    Result folder detailed

--- utils/initializator.go
+++ utils/initializator.go
@@ -12,7 +12,9 @@ import (
 func Initialization(folderName string) {
 	newPath := filepath.Join(ResultsPath, folderName)
 	if _, err := os.Stat(newPath); errors.Is(err, os.ErrNotExist) {
-		err := os.MkdirAll(newPath, os.ModePerm)
+		err := os.MkdirAll(filepath.Join(newPath, "bin"), os.ModePerm)
+		err = os.MkdirAll(filepath.Join(newPath, "log"), os.ModePerm)
+		err = os.MkdirAll(filepath.Join(newPath, "results"), os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -21,11 +23,12 @@ func Initialization(folderName string) {
 		fmt.Println("Folder Exist. Would you like to delete and generate again ?")
 		fmt.Println("[1] For Yes, Enter 1")
 		fmt.Println("[2] For No,  Enter 2")
-		var answer string
-		answer = "1"
+		var answer = "1"
 		if answer == "1" {
 			os.RemoveAll(newPath)
-			err = os.MkdirAll(newPath, os.ModePerm)
+			err := os.MkdirAll(filepath.Join(newPath, "bin"), os.ModePerm)
+			err = os.MkdirAll(filepath.Join(newPath, "log"), os.ModePerm)
+			err = os.MkdirAll(filepath.Join(newPath, "results"), os.ModePerm)
 			if err != nil {
 				log.Fatal(err)
 			}
