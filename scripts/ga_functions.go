15c24a6d94668d97d1ad7aebf30efb8a82d4dd91
Burak Tagtekin <burak.tagtekin@icloud.com>
2022-01-05 22:41:19 +0300

    Execution Function Changed

--- scripts/ga_functions.go
+++ scripts/ga_functions.go
@@ -1,7 +1,7 @@
 package scripts
 
 import (
-	"fmt"
+	"bytes"
 	"ga_tuner/utils"
 	"log"
 	"math/rand"
@@ -10,8 +10,6 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
-
-	uuid "github.com/satori/go.uuid"
 )
 
 func MutNormalFloat64(genome []float64, rate float64, rng *rand.Rand) {
@@ -29,36 +27,39 @@ func MutNormalFloat64(genome []float64, rate float64, rng *rand.Rand) {
 }
 
 // Returns a Binary Vector with just only 0 - 1.
-func InitBinaryFloat64(n uint, lower, upper float64, rng *rand.Rand) (floats []float64) {
+func InitBinaryFloat64(n uint, lower, upper float64, rng *rand.Rand) (FlagMap map[string]float64, floats []float64) {
 	floats = make([]float64, n)
+	FlagMap = make(map[string]float64)
 	for i := range floats {
 		floats[i] = lower + float64(rng.Intn(int(upper)))
-		// fmt.Printf("Generated Number is %f\n", floats[i])
+		FlagMap[availableFlags[i]] = floats[i]
 	}
 	return
 }
 
-func CompileCode(cmd string, id uuid.UUID) float64 {
+func CompileCode(cmd string, id string) (Total float64) {
 	// COMPILE
 	command := strings.Split(cmd, " ")
 	app := os.Args[2]
-	out, err := exec.Command(app, command...).Output()
+	out_compile, err := exec.Command(app, command...).Output()
 	if err != nil {
-		log.Print(string(out))
+		log.Print(string(out_compile))
 		log.Fatal(err)
 	}
 
 	// EXECUTION
-	start := time.Now()
-	app = "cmd"
-	exec_file := filepath.Join(utils.ResultsPath, os.Args[1], id.String())
-	out, err = exec.Command(exec_file).Output()
-	if err != nil {
-		log.Print(string(out))
-		log.Fatal(err)
-	}
-	elapsed := time.Since(start).Seconds()
-	fmt.Printf("Total Elapsed Time is : %f s\n", elapsed)
+	exec_file := filepath.Join(utils.ResultsPath, os.Args[1], "bin", id)
 
-	return float64(elapsed)
+	command_exec := exec.Command(exec_file)
+	var out_exec bytes.Buffer
+	// set the output to our variable
+	command_exec.Stdout = &out_exec
+	start := time.Now()
+	err = command_exec.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	Total = time.Since(start).Seconds()
+	return
 }
