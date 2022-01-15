a2b6f0f41c2e49c9dfccfcac5c5cd8a505bfb888
Burak Tagtekin <burak.tagtekin@icloud.com>
2022-01-15 18:01:47 +0300

    Particle Swarm Optimizations Added

--- scripts/pso_functions.go
+++ scripts/pso_functions.go
@@ -1,30 +1,49 @@
 package scripts
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
-	"runtime"
 
 	"github.com/MaxHalford/eaopt"
 	uuid "github.com/satori/go.uuid"
 )
 
 func PSORunner() {
-	fmt.Printf("Particle Swarm Optimization running parallel with %d Processor\n", runtime.NumCPU())
-	var spso, err = eaopt.NewSPSO(50, 10, 0, 1, 0, true, nil)
-	f := WriteJson(spso.GA)
+	var spso, err = eaopt.NewSPSO(10, 10, 0, 1, 0, true, nil)
+
+	f, _ := os.Create(os.Args[3])
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	fmt.Fprint(w, "")
+	w.Flush()
+
+	f, _ = os.OpenFile(os.Args[3], os.O_APPEND|os.O_WRONLY, 0666)
+	defer f.Close()
+
+	var bytes, _ = json.Marshal(spso.GA)
+	f.WriteString(string(bytes) + "\n")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	// Collecting Baselines of Code
+	level_two := CollectBaseline("O2")
+	level_three := CollectBaseline("O3")
+
+	fmt.Printf("O2 BASELINE IS : %f\n", level_two)
+	fmt.Printf("O3 BASELINE IS : %f\n", level_three)
 
 	// Fix random number generation
 	spso.GA.RNG = rand.New(rand.NewSource(42))
 	spso.GA.Callback = func(ga *eaopt.GA) {
-		fmt.Printf("Best fitness at generation %d: ID:  %s, Fitness : %f", ga.Generations, ga.HallOfFame[0].ID, ga.HallOfFame[0].Fitness)
-		var bytes, _ = json.Marshal(ga)
+		fmt.Printf("Best fitness at generation %d: ID:  %s, Fitness : %f, Improvement : %f\n", ga.Generations, ga.HallOfFame[0].ID, ga.HallOfFame[0].Fitness, 1-ga.HallOfFame[0].Fitness/level_two)
+		var bytes, err = json.Marshal(ga)
+		if err != nil {
+			fmt.Println(err)
+		}
 		f.WriteString(string(bytes) + "\n")
 	}
 	_, y, err := spso.Minimize(FitnessFunction, 50)
@@ -37,20 +56,12 @@ func PSORunner() {
 
 func FitnessFunction(X []float64) (y float64) {
 	output := uuid.NewV4().String()
-	flattenSlice := make([]float64, 0)
-	for _, v := range X {
-		if v < 0.5 {
-			flattenSlice = append(flattenSlice, 0)
-		} else {
-			flattenSlice = append(flattenSlice, 1)
-		}
-	}
-	cmd, _ := MatchBinaryWithFlags(flattenSlice)
+	cmd, _ := MatchBinaryWithFlags(X, "O2")
 
 	// Adding some polybench information to run cmd
 	cmd = addPolybenchDependencies(cmd, os.Args[1], output)
 
 	// Total is Execution time of Code.
-	total := CompileCode(cmd, output)
+	total := CompileCode(cmd, output, 3)
 	return total
 }
